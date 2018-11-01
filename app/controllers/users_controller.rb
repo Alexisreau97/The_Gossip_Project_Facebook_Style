a4674ad7850d2094be13383515681ae6fe1e9dce
Alexisreau97 <alexis.reaupro@gmail.com>
2018-11-01 15:04:32 +0100

    add session home

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,23 @@
 class UsersController < ApplicationController
+  def home
+    if session[:user_id]
+      @current_user = User.find(session[:user_id])
+    end
+  end
+
   def login
   end
 
   def check
-    @current_user = User.where(email: params[:email], password: params[:password]).first
+    @current_user = User.where(email: params[:email], password: params[:password]).last
     if @current_user
-      flash[:info] = "Bienvenue #{@current_user.name} !"
-      redirect_to "/gossips"
+      session[:user_id] = @current_user.id
+      flash[:info] = "Vous êtes maintenant connecté"
+      redirect_to "/users/home"
     else
+      session[:user_id] = nil
       flash[:info] = "Échec de la connexion"
       redirect_to "/users/login"
     end
   end
 end
-
-
